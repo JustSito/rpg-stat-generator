ce5dd699f0eaeeaca00b31d8916f6a53bed27452
SITO <sitoaugustine@gmail.com>
2026-06-09 14:21:30 +0100

    Initial commit: add RPG stat generator

---
+++ stat-generator.py
@@ -0,0 +1,27 @@
+def create_character(name, strength, intelligence, charisma):
+    full_dot = '●'
+    empty_dot = '○'
+    if not isinstance(name, str):
+        return 'The character name should be a string'
+    if name == "":
+        return "The character should have a name"
+    if len(name) > 10:
+        return "The character name is too long"
+    if " " in name:
+        return "The character name should not contain spaces"
+    
+    if not isinstance(strength, int) or not isinstance(intelligence, int) or not isinstance(charisma, int):
+        return 'All stats should be integers'
+    if strength < 1 or intelligence < 1 or charisma < 1:
+        return "All stats should be no less than 1"
+    if strength > 4 or intelligence > 4 or charisma > 4:
+        return "All stats should be no more than 4"
+    if sum([strength, intelligence, charisma]) != 7:
+        return 'The character should start with 7 points'
+    
+    def stat_bar(value):
+        return full_dot * value + (empty_dot * (10 - value))
+    
+    return f"{name}\nSTR {stat_bar(strength)}\nINT {stat_bar(intelligence)}\nCHA {stat_bar(charisma)}"
+
+print(create_character('ren', 4, 2, 1))
